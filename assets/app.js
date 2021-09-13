eb108ed9204d5b39bd523b892d4877fdd973976d
AmandineFloucaut <afloucaut@gmail.com>
2021-09-13 11:02:11 +0200

    preparation shifumi object ok

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,31 @@
 console.log('%c' + "app.js chargé", 'color: #0bf; font-size: 1rem; background-color:#fff');
+
+const shifumi = {
+    possibilities : [
+        'pierre.png',
+        'feuille.png',
+        'ciseaux.png'
+    ],
+
+    turn : 0,
+    pointsUser : 0,
+    pointsComputer : 0,
+
+    playPossibilities : document.getElementById('play-possibilities'),
+
+    winnerText : document.querySelector('.game--winner p'),
+    endGameText : document.querySelector('.game--winner span'),
+
+    imgUserValidatedElement : document.querySelector('.game__user--img'),
+    imgComputerValidatedElement : document.querySelector('.game__computer--img'),
+
+    buttonPlayAgain : document.getElementById('play-again'),
+
+}
+console.log(typeof shifumi);
+console.log(shifumi.playPossibilities);
+console.log(shifumi.winnerText);
+console.log(shifumi.endGameText);
+console.log(shifumi.imgUserValidatedElement);
+console.log(shifumi.imgComputerValidatedElement);
+console.log(shifumi.buttonPlayAgain);
