fb7427616110ab7a98971a882e183ca1a9d06731
AmandineFloucaut <afloucaut@gmail.com>
2022-02-04 10:34:58 +0100

    refacto app.js

--- assets/app.js
+++ assets/app.js
@@ -39,8 +39,8 @@ const shifumi = {
      * Initialize function
      */
     init : function(){
-        // Debug
-        console.log('init function');
+        // Debug init
+        //console.log('init function');
 
         let buttonElement = document.getElementById('arrow');
 
@@ -57,8 +57,12 @@ const shifumi = {
         shifumi.buttonPlayAgain.addEventListener('click', shifumi.handlePlayAgain);
     },
 
+    /**
+     * Start the game at click on "JOUER" button
+     * @param {} event
+     */
     handleStartGame : function(event){
-        // Debug
+        // Debug StartGame
         console.log('function handle stard game');
 
         if(shifumi.gameContainer.classList.contains("game_container--hidden")){
@@ -84,13 +88,142 @@ const shifumi = {
      * @param {string} computerChoice : name picture
      */
     handleWinnerGame : function(userChoice, computerChoice){
-        // Debug
-        console.log('handle winner game function');
+        // Debug WinnerGame
+        //console.log('handle winner game function');
 
         let userChoiceValidated = shifumi.userChoice(userChoice);
-
         let computerChoiceValidated = shifumi.computerChoice(computerChoice);
 
+        // Function define below
+        shifumi.checkWinnerAtRound(userChoiceValidated, computerChoiceValidated);
+        shifumi.endGame(shifumi.pointsUser, shifumi.pointsComputer);
+    },
+
+    /**
+     * Function for play again when user click on the button
+     * @param {click} event : click on button play again
+     */
+    handlePlayAgain : function(event){
+        // Debug PlayAgain
+        //console.log('play again function');
+
+        // Remove all pictures choice validate at the previous party
+        while (shifumi.userChoicesContainer.firstChild) {
+            shifumi.userChoicesContainer.removeChild(shifumi.userChoicesContainer.firstChild);
+        }
+
+        while (shifumi.computerChoicesContainer.firstChild) {
+            shifumi.computerChoicesContainer.removeChild(shifumi.computerChoicesContainer.firstChild);
+        }
+
+        // Define below
+        shifumi.resetElementForNewGame();
+        shifumi.resetImgValidateElement();
+    },
+
+    /**
+     * Function for compare points user and computer and for stop the game
+     * @param {int} pointsUser
+     * @param {int} pointsComputer
+     */
+    endGame : function(pointsUser, pointsComputer){
+        // Debug endGame
+        //console.log("end game function");
+
+        if(pointsUser == 5 || pointsComputer == 5){
+
+            shifumi.playPossibilities.className = "play__possibilities--hidden";
+
+            shifumi.buttonPlayAgain.className = "play-again--display";
+
+            shifumi.resetImgValidateElement();
+
+            // Add class endGame for update img display (in block)
+            let imgValidatedElement = document.querySelectorAll('.choice__validated--img');
+
+            for(let img of imgValidatedElement) {
+                img.classList.add('endGame');
+            }
+
+            shifumi.roundGameText.textContent = "Partie terminée !"
+
+            if(pointsUser == 5){
+                shifumi.winnerText.textContent = "Bravo tu as battu l'ordinateur en " + shifumi.turn + " round !";
+            }
+            else {
+                shifumi.winnerText.textContent = "L'ordinateur t'a battu en " + shifumi.turn + " round, essaie encore !";
+            }
+        }
+    },
+
+    /**
+     * Function to retrieve choice user validated
+     * @param {click user} event
+     * @return userChoice (just name of picture)
+     */
+    userChoice : function(event){
+        // Debug userChoice
+        //console.log('user choice function');
+
+        let imgElementChoice = event.currentTarget;
+
+        let imageSource = imgElementChoice.src;
+
+        // When user click on one picture, insert this in html for display game validated
+        shifumi.imgUserValidatedElement.src = imageSource;
+
+        // Create img element and insert in src attribut the user choice (where he's click)
+        let imgChoiceElement = document.createElement("img");
+        imgChoiceElement.src = shifumi.imgUserValidatedElement.src;
+
+        // Insert class name for css and insert in container
+        imgChoiceElement.className = "choice__validated--img";
+        shifumi.userChoicesContainer.appendChild(imgChoiceElement);
+
+        // Retrieve just name of touch image for comparison with computer choice and return this
+        // WARNING : in dev environment, slice(36)
+        let userChoice = imageSource.slice(36);
+
+        // WARNING : in prod environment, slice(54)
+        //let userChoice = imageSource.slice(54);
+
+        return userChoice;
+    },
+
+    /**
+     * Function for define random choice computeur
+     * @param {string} computerChoice
+     * @return computerChoice
+     */
+    computerChoice : function(computerChoice){
+        // Debug computerChoice
+        //console.log('computer choice function');
+
+        // Generate random number for use this like index of possibilities array
+        // DOC https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Global_Objects/Math/floor && https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+        let randomIndex = Math.floor(Math.random()*shifumi.possibilities.length);
+        computerChoice = shifumi.possibilities[randomIndex];
+
+        // Define src of computer choice validated img (relative path)
+        shifumi.imgComputerValidatedElement.src = "assets/img/" + computerChoice;
+
+        // Create img element and insert in src attribut the computer choice (where he's click)
+        let imgChoiceElement = document.createElement("img");
+        imgChoiceElement.src = shifumi.imgComputerValidatedElement.src;
+        // Insert class name for css and insert in container
+        imgChoiceElement.className = "choice__validated--img";
+        shifumi.computerChoicesContainer.appendChild(imgChoiceElement);
+
+        // return name of picture choice by computer (for comparate with userChoice)
+        return computerChoice;
+    },
+
+    /**
+     * Method for check winner at each round
+     * @param {string} userChoiceValidated
+     * @param {string} computerChoiceValidated
+     */
+    checkWinnerAtRound: function(userChoiceValidated, computerChoiceValidated){
         if(userChoiceValidated ===  computerChoiceValidated){
 
             shifumi.turn += 1;
@@ -121,36 +254,18 @@ const shifumi = {
                 shifumi.winnerText.textContent = "Tu gagnes !";
                 shifumi.pointsUserElement.textContent = shifumi.pointsUser;
             }
-
-        // Function define line 83
-        shifumi.endGame(shifumi.pointsUser, shifumi.pointsComputer);
     },
 
     /**
-     * Function for play again when user click on the button
-     * @param {click} event : click on button play again
-     */
-    handlePlayAgain : function(event){
-
-        console.log('play again function');
-
-        while (shifumi.userChoicesContainer.firstChild) {
-            shifumi.userChoicesContainer.removeChild(shifumi.userChoicesContainer.firstChild);
-        }
-
-        while (shifumi.computerChoicesContainer.firstChild) {
-            shifumi.computerChoicesContainer.removeChild(shifumi.computerChoicesContainer.firstChild);
-        }
-
+    * Reset class button play and container playPossibilities, textContent of round and winner element
+    */
+    resetElementForNewGame: function(){
         shifumi.buttonPlayAgain.className = "play-again--hidden";
 
         shifumi.playPossibilities.className = "play__possibilities--display";
 
         shifumi.roundGameText.textContent = "";
         shifumi.winnerText.textContent = "";
-
-        shifumi.imgUserValidatedElement.src = "";
-        shifumi.imgComputerValidatedElement.src = "";
 
         shifumi.pointsUserElement.textContent = "0";
         shifumi.pointsComputerElement.textContent = "0";
@@ -161,121 +276,11 @@ const shifumi = {
     },
 
     /**
-     * Function for compare points user and computer and for stop the game
-     * @param {int} pointsUser
-     * @param {int} pointsComputer
+     * Reset src img validte by user and computer
      */
-    endGame : function(pointsUser, pointsComputer){
-
-        console.log("end game function");
-
-        if(pointsUser == 5 || pointsComputer == 5){
-
-            shifumi.playPossibilities.className = "play__possibilities--hidden";
-
-            shifumi.buttonPlayAgain.className = "play-again--display";
-
-            shifumi.imgUserValidatedElement.src = "";
-            shifumi.imgComputerValidatedElement.src = "";
-
-            // Add class endGame for update img display (in block)
-            let imgValidatedElement = document.querySelectorAll('.choice__validated--img');
-
-            for(let img of imgValidatedElement) {
-                img.classList.add('endGame');
-            }
-
-            shifumi.roundGameText.textContent = "Partie terminée !"
-
-            if(pointsUser == 5){
-                shifumi.winnerText.textContent = "Bravo tu as battu l'ordinateur en " + shifumi.turn + " round !";
-            }
-            else {
-                shifumi.winnerText.textContent = "L'ordinateur t'a battu en " + shifumi.turn + " round, essaie encore !";
-            }
-        }
-    },
-
-    displayIntroContainer : function(){
-
-        for(let container of shifumi.presentationContainers){
-
-            container.classList.remove("intro--display");
-            container.classList.add("intro--hidden");
-
-        }
-    },
-
-    hiddenIntroContainer : function(){
-        for(let container of shifumi.presentationContainers){
-
-            container.classList.remove("intro--hidden");
-            container.classList.add("intro--display");
-        }
-    },
-
-    /**
-     * Function to retrieve choice user validated
-     * @param {click user} event
-     * @returns userChoice (just name of picture)
-     */
-    userChoice : function(event){
-        // Debug
-        //console.log('user choice function');
-
-        let imgElementChoice = event.currentTarget;
-
-        let imageSource = imgElementChoice.src;
-
-        // When user click on one picture, insert this in html for display game validated
-        shifumi.imgUserValidatedElement.src = imageSource;
-
-        // Create img element and insert in src attribut the user choice (where he's click)
-        let imgChoiceElement = document.createElement("img");
-        imgChoiceElement.src = shifumi.imgUserValidatedElement.src;
-
-        // Insert class name for css and insert in container
-        imgChoiceElement.className = "choice__validated--img";
-        shifumi.userChoicesContainer.appendChild(imgChoiceElement);
-
-        // Retrieve just name of touch image for comparison with computer choice and return this
-        // WARNING : in dev environment, slice(36)
-        let userChoice = imageSource.slice(36);
-
-        // WARNING : in prod environment, slice(54)
-        //let userChoice = imageSource.slice(54);
-
-        shifumi.displayIntroContainer();
-
-        return userChoice;
-    },
-
-    /**
-     * Function for define random choice computeur
-     * @param {string} computerChoice
-     * @returns
-     */
-    computerChoice : function(computerChoice){
-        // Debug
-        //console.log('computer choice function');
-
-        // Generate random number for use this like index of possibilities array
-        // DOC https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Global_Objects/Math/floor && https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-        let randomIndex = Math.floor(Math.random()*shifumi.possibilities.length);
-        computerChoice = shifumi.possibilities[randomIndex];
-
-        // Define src of computer choice validated img (relative path)
-        shifumi.imgComputerValidatedElement.src = "assets/img/" + computerChoice;
-
-        // Create img element and insert in src attribut the computer choice (where he's click)
-        let imgChoiceElement = document.createElement("img");
-        imgChoiceElement.src = shifumi.imgComputerValidatedElement.src;
-        // Insert class name for css and insert in container
-        imgChoiceElement.className = "choice__validated--img";
-        shifumi.computerChoicesContainer.appendChild(imgChoiceElement);
-
-        // return name of picture choice by computer (for comparate with userChoice)
-        return computerChoice;
+    resetImgValidateElement: function(){
+        shifumi.imgUserValidatedElement.src = "";
+        shifumi.imgComputerValidatedElement.src = "";
     }
 }
 
